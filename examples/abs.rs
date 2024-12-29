01be1fb5bdffefd191c8822cce892eeb9b9a2404
daddinuz <daddinuz@gmail.com>
2024-12-30 00:16:12 +0100

    fmt

--- examples/abs.rs
+++ examples/abs.rs
@@ -1,7 +1,7 @@
 use cat::apply::Apply;
 use cat::builtin::*;
-use cat::quote::Quote;
 use cat::flow;
+use cat::quote::Quote;
 
 fn main() {
     let abs = flow![dup, 0, lt, Quote(flow![neg]), r#if];
--- examples/handshake.rs
+++ examples/handshake.rs
@@ -1,7 +1,7 @@
 use cat::apply::Apply;
 use cat::builtin::*;
-use cat::quote::Quote;
 use cat::flow;
+use cat::quote::Quote;
 
 fn main() {
     let client = flow![
